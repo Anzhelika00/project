db3341b0745dabf8dd5cca46297aef7c3fef085d
Anzhelika00 <sadovskay1711@gmail.com>
2024-12-26 22:35:59 +0300

    Create dealer.cpp

---
+++ dealer.cpp
@@ -0,0 +1,80 @@
+#include "../Downloads/dealer.hpp"
+using namespace std;
+
+namespace dealer
+{
+    int get_random_card()
+    {
+        return 1 + (rand() % 10);
+    }
+
+    vector<int> generate_dealer_cards()
+    {
+        vector<int> cards = { get_random_card(), get_random_card() };
+
+        while (get_card_values(cards) < 17)
+        {
+            cards.push_back(get_random_card());
+        }
+
+        return cards;
+    }
+
+    int get_card_values(vector<int> cards)
+    {
+        int total = 0;
+
+        for (const int card : cards)
+        {
+            total += card;
+        }
+
+        return total;
+    }
+
+    void display_card_value(int card)
+    {
+        if (card == 1)
+        {
+            cout << "A\n";
+        }
+        else if (card > 1 && card < 10)
+        {
+            cout << card << '\n';
+        }
+        else
+        {
+            int chance = rand() % 101;
+
+            if (chance < 10)
+            {
+                cout << "10\n";
+            }
+            else if (chance < 30)
+            {
+                cout << "J\n";
+            }
+            else if (chance < 60)
+            {
+                cout << "Q\n";
+            }
+            else
+            {
+                cout << "K\n";
+            }
+        }
+    }
+
+
+    void print_dealer_cards(vector<int> cards, int dealer_total)
+    {
+
+        cout << " арты дилера : \n";
+
+        for (const int card : cards)
+        {
+            dealer::display_card_value(card);
+        }
+        cout << "—умма дилера: " << dealer_total << '\n';
+    }
+}
